add jacobian. correct angles

reacher/reacher_kinematics.py
@@ -17,7 +17,7 @@ def calculate_forward_kinematics_robot(joint_angles):
     Returns:
       xyz coordinates of the end-effector in the arm frame. Numpy array of 3 elements.
     """
-
+    # TODO for students: Implement this function. ~25-35 lines of code.
     end_effector_xyz = np.array([0.0, 0.0, 0.0])
     return end_effector_xyz
 
@@ -33,8 +33,30 @@ def ik_cost(end_effector_pos, guess):
     Returns:
       Euclidean distance between end_effector_pos and guess. Returns float.
     """
+    # TODO for students: Implement this function. ~1-5 lines of code.
     cost = 0.0
     raise cost
+
+def calculate_jacobian(joint_angles):
+    """Calculate the jacobian of the end-effector position wrt joint angles.
+    
+    Calculate the jacobian, which is a matrix of the partial derivatives
+    of the forward kinematics with respect to the joint angles 
+    arranged as such:
+    
+    dx/dtheta1 dx/dtheta2 dx/dtheta3
+    dy/dtheta1 dy/dtheta2 dy/dtheta3
+    dz/dtheta1 dz/dtheta2 dz/dtheta3
+    
+    Args:
+      joint_angles: joint angles of robot arm. Numpy array of 3 elements.
+    
+    Returns:
+      Jacobian matrix. Numpy 3x3 array.
+    """
+    # TODO for students: Implement this function. ~5-10 lines of code.
+    jacobian = np.zeros(3, 3)
+    return jacobian
 
 def calculate_inverse_kinematics(end_effector_pos, guess):
     """Calculates joint angles given desired xyz coordinates.
@@ -42,7 +64,8 @@ def calculate_inverse_kinematics(end_effector_pos, guess):
     Use gradient descent to minimize the inverse kinematics loss function. The
     joint angles that minimize the loss function are the joint angles that give 
     the smallest error from the actual resulting end-effector position to the
-    desired end-effector position.
+    desired end-effector position. You should use the jacobain function
+    you wrote above.
 
     Args:
       end_effector_pos: Desired xyz coordinates of end-effector. Numpy array of 3 elements.
@@ -50,5 +73,6 @@ def calculate_inverse_kinematics(end_effector_pos, guess):
     Returns:
       Joint angles that correspond to given desired end-effector position. Numpy array with 3 elements.
     """
+    # TODO for students: Implement this function. ~10-20 lines of code.
     joint_angles = np.array([0.0, 0.0, 0.0])
     return joint_angles

reacher/reacher_manual_control.py
@@ -1,4 +1,4 @@
-# from reacher import reacher_kinematics
+from reacher import reacher_kinematics
 import pybullet as p
 import reacher.data as pd
 import time
@@ -23,35 +23,38 @@ HIP_OFFSET = 0.0335
 L1 = 0.08
 L2 = 0.11
 
-def main(argv):
-  run_on_robot = FLAGS.run_on_robot
-  p.connect(p.GUI)
+def configure_pybullet():
   p.configureDebugVisualizer(p.COV_ENABLE_RGB_BUFFER_PREVIEW, 0)
   p.configureDebugVisualizer(p.COV_ENABLE_DEPTH_BUFFER_PREVIEW, 0)
   p.configureDebugVisualizer(p.COV_ENABLE_SEGMENTATION_MARK_PREVIEW, 0)
   p.setAdditionalSearchPath(pybullet_data.getDataPath())
-  p.resetDebugVisualizerCamera(cameraDistance=0.3, cameraYaw=-134, cameraPitch=-30, cameraTargetPosition=[0,0,0.1])
+  p.resetDebugVisualizerCamera(cameraDistance=0.3, cameraYaw=-200, cameraPitch=-30, cameraTargetPosition=[0,0,0.1])
+
+def main(argv):
+  run_on_robot = FLAGS.run_on_robot
+  p.connect(p.GUI)
+  configure_pybullet()
   
   URDF_PATH = pd.getDataPath() + "/pupper_arm.urdf"
-  print(URDF_PATH)
-  print("****************")
   reacher = p.loadURDF(URDF_PATH, useFixedBase=True)
+  target_visual_shape = p.createVisualShape(p.GEOM_SPHERE, radius=0.015)
+  target_position = np.array([0,0,0])
+  sphere_id = p.createMultiBody(baseVisualShapeIndex=target_visual_shape, basePosition=target_position)
 
-  jointIds = []
-  paramIds = []
+  joint_ids = []
+  param_ids = []
 
-  p.setPhysicsEngineParameter(numSolverIterations=10)
+  p.setPhysicsEngineParameter(numSolverIterations=10) # Affects performance?
   p.changeDynamics(reacher, -1, linearDamping=0, angularDamping=0)
 
   for j in range(p.getNumJoints(reacher)):
     p.changeDynamics(reacher, j, linearDamping=0, angularDamping=0)
     info = p.getJointInfo(reacher, j)
-    #print(info)
     jointName = info[1]
     jointType = info[2]
     if (jointType == p.JOINT_PRISMATIC or jointType == p.JOINT_REVOLUTE):
-      jointIds.append(j)
-      paramIds.append(p.addUserDebugParameter(jointName.decode("utf-8"), -math.pi, math.pi, 0))
+      joint_ids.append(j)
+      param_ids.append(p.addUserDebugParameter(jointName.decode("utf-8"), -math.pi, math.pi, 0))
 
   if run_on_robot:
     if platform == "linux" or platform == "linux2":
@@ -67,24 +70,27 @@ def main(argv):
   counter = 0
   while (1):
     counter += 1
-    joint_angles = [0, 0, 0]
-    for i in range(len(paramIds)):
-      c = paramIds[i]
-      targetPos = p.readUserDebugParameter(c)
-      joint_angles[i] = targetPos
-      p.setJointMotorControl2(reacher, jointIds[i], p.POSITION_CONTROL, targetPos, force=2.)
+    joint_angles = np.zeros(3)
+
+    # Read 
+    for i in range(len(param_ids)):
+      c = param_ids[i]
+      target_pos = p.readUserDebugParameter(c)
+      joint_angles[i] = target_pos
+      p.setJointMotorControl2(reacher, joint_ids[i], p.POSITION_CONTROL, target_pos, force=20.)
 
     if run_on_robot:
       full_actions = np.zeros([3, 4])
-      full_actions[:, 2] = np.reshape(joint_angles, 3)
-      hardware_interface.set_joint_space_parameters(kp=KP,
-                                                    kd=KD,
-                                                    max_current=MAX_CURRENT)
+      full_actions[:, 3] = np.reshape(joint_angles, 3)
       hardware_interface.set_actuator_postions(np.array(full_actions))
-
-    if counter % 5 == 0:
-      pass
-      # print(reacher_kinematics.calculate_forward_kinematics_robot(joint_angles))
+    
+    # Use forward kinematics to set the position of the sphere to the
+    # xyz coordinates computed by your forward kinematics code.
+    # If it's working, the sphere should overlap with the 
+    # actual robot end-effector. There may be some lag due to the 
+    # inertia of the robot arm.
+    end_effector_pos = reacher_kinematics.calculate_forward_kinematics_robot(joint_angles)
+    p.resetBasePositionAndOrientation(sphere_id, posObj=end_effector_pos, ornObj=[0, 0, 0, 1])
     time.sleep(0.01)
 
 app.run(main)