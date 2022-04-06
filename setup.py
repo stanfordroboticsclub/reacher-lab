668c1a87d9d57313174d4be6c7c76517236364ff
Nathan Kau <nathankau@gmamil.com>
2022-04-05 17:35:39 -0700

    remove refs to pupper

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The DJIPupperSim Authors.
+# Copyright 2021 The Reacher Sim Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""setup.py for DJIPupperSim.
+"""setup.py for Reacher Sim.
 
 Install for development:
 
